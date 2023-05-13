392aa07b711388d86076eaabe3d3ef7bfe08a2a2
Sarah Bunker <ChemEnCoder@gmail.com>
2023-05-13 15:34:36 -0600

    adding database, repo, employee, main method, and changing route api

---
+++ src/main/java/com/sarah/payroll/ReactAndSpringDataRestApplication.java
@@ -0,0 +1,7 @@
+@SpringBootApplication
+public class ReactAndSpringDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactAndSpringDataRestApplication.class, args);
+	}
+}
