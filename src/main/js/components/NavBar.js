76f2938d7a237c54c986e502489e9ceb3cf91abb
Sarah Bunker <ChemEnCoder@gmail.com>
2023-05-17 16:34:01 -0600

    updating nav bar styling

--- src/main/js/components/NavBar.js
+++ src/main/js/components/NavBar.js
@@ -1,46 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function NavBar({ links }) {
-  const navLinks = [];
+  const [activeTooltipIndex, setActiveTooltipIndex] = useState(-1);
 
-  if ("first" in links) {
-    const linkUrl = links.first.href;
-    navLinks.push(
-      <a key="first" href={linkUrl} className="nav-link">
-        &lt;&lt;
-        <span className="tooltip">First Page</span>
-      </a>
-    );
-  }
-  if ("prev" in links) {
-    const linkUrl = links.prev.href;
-    navLinks.push(
-      <a key="prev" href={linkUrl} className="nav-link">
-        &lt;
-        <span className="tooltip">Previous Page</span>
-      </a>
-    );
-  }
-  if ("next" in links) {
-    const linkUrl = links.next.href;
-    navLinks.push(
-      <a key="next" href={linkUrl} className="nav-link">
-        &gt;
-        <span className="tooltip">Next Page</span>
-      </a>
-    );
-  }
-  if ("last" in links) {
-    const linkUrl = links.last.href;
-    navLinks.push(
-      <a key="last" href={linkUrl} className="nav-link">
-        &gt;&gt;
-        <span className="tooltip">Last Page</span>
-      </a>
-    );
-  }
+  const handleMouseEnter = (index) => {
+    setActiveTooltipIndex(index);
+  };
 
-  return <div className="nav-links">{navLinks}</div>;
+  const handleMouseLeave = () => {
+    setActiveTooltipIndex(-1);
+  };
+
+  const navLinks = [
+    { key: "first", text: "<<", tooltip: "First Page", href: links.first?.href },
+    { key: "prev", text: "<", tooltip: "Previous Page", href: links.prev?.href },
+    { key: "next", text: ">", tooltip: "Next Page", href: links.next?.href },
+    { key: "last", text: ">>", tooltip: "Last Page", href: links.last?.href }
+  ];
+
+  return (
+    <div className="nav-links">
+      {navLinks.map(({ key, text, tooltip, href }, index) => (
+        <a
+          key={key}
+          href={href}
+          className="nav-button"
+          onMouseEnter={() => handleMouseEnter(index)}
+          onMouseLeave={handleMouseLeave}
+        >
+          {text}
+          {activeTooltipIndex === index && <span className="tooltip">{tooltip}</span>}
+        </a>
+      ))}
+    </div>
+  );
 }
 
 export default NavBar;
